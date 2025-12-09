988819c1fc505076ad5f972d7f6158425915400e
eimamura <erielcio00@gmail.com>
2025-12-09 20:52:59 -0300

    feat: introduce concurrent goroutine with WaitGroup and time delay

--- main.go
+++ main.go
@@ -4,10 +4,24 @@ import (
 	"fmt"
 	"hello/greetings"
 	"rsc.io/quote"
+	"sync"
+	"time"
 )
 
 func main() {
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 3; i++ {
+			fmt.Println("Goroutine:", i)
+			time.Sleep(100 * time.Millisecond)
+		}
+	}()
+
 	fmt.Println(quote.Hello())
 	message := greetings.Hello2("Gladys")
 	fmt.Println(message)
+
+	wg.Wait()
 }
